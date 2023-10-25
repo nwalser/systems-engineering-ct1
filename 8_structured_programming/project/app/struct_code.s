2b0f6d9a37f5adf7e122537063f485a6091365e1
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-10-25 22:59:42 +0200

    added lab 8 solutions

--- 8_structured_programming/project/app/struct_code.s
+++ 8_structured_programming/project/app/struct_code.s
@@ -30,8 +30,14 @@ ADDR_7_SEG_BIN_DS3_0      EQU        0x60000114
 ADDR_LCD_COLOUR           EQU        0x60000340
 ADDR_LCD_ASCII            EQU        0x60000300
 ADDR_LCD_ASCII_BIT_POS    EQU        0x60000302
-ADDR_LCD_ASCII_2ND_LINE   EQU        0x60000314
+ADDR_LCD_ASCII_2ND_LINE   EQU        0x60000330
 
+LCD_COLOR_ON 			  EQU 	     0xFFFF
+LCD_COLOR_OFF			  EQU 	     0x0000
+
+LCD_R_OFFSET			  EQU 	     0x0
+LCD_G_OFFSET			  EQU 	     0x2
+LCD_B_OFFSET			  EQU 	     0x4
 
 ; ------------------------------------------------------------------
 ; -- Program-Defines
@@ -76,14 +82,146 @@ main    PROC
         BL         clear_lcd
 
 main_loop
-; STUDENTS: To be programmed
+		BL adc_get_value ; function overwrites other registers, this is the reason why i use it first
+		MOVS R2, R0 ; R2: store adc value
+		
+		LDR R0, =ADDR_BUTTONS
+		LDRB R1, [R0]
+		MOVS R4, #1
+		ANDS R1, R1, R4 ; R1: store T0 value
+		
+		LDR R0, =ADDR_DIP_SWITCH_7_0
+		LDRB R3, [R0]
+		SUBS R3, R3, R2 ; R3: store subtracted adc value
 
+		; test for cases
+		CMP R1, #0
+		BEQ case_green
+		
+		CMP R3, #0
+		BGE case_blue
+		
+		B case_red
+		
+		
+; ----------------------------------------
+case_green
+		; change display color
+		LDR R0, =ADDR_LCD_COLOUR
+		LDR R4, =LCD_COLOR_ON
+		STRH R4, [R0, #LCD_G_OFFSET]
+		LDR R4, =LCD_COLOR_OFF
+		STRH R4, [R0, #LCD_R_OFFSET]
+		STRH R4, [R0, #LCD_B_OFFSET]
 
+		; display on seven segment display
+		LDR R0, =ADDR_7_SEG_BIN_DS3_0
+		STRB R2, [R0]
 
+; create led mask
+		LSRS R2, #3 ; divison by 8
+		MOVS R3, #1
+		MOVS R4, #1
+create_led_mask_loop
+		CMP R2, #0
+		BEQ create_led_mask_loop_end
+		
+		LSLS R3, #1
+		ORRS R3, R3, R4
+		SUBS R2, R2, #1 ; decrement counter
+		
+		B create_led_mask_loop
+create_led_mask_loop_end
+		LDR R0, =ADDR_LED_31_0
+		STR R3, [R0]
 
-; END: To be programmed
         B          main_loop
-        
+
+
+; ----------------------------------------
+case_blue
+		; change display color
+		LDR R0, =ADDR_LCD_COLOUR
+		LDR R4, =LCD_COLOR_ON
+		STRH R4, [R0, #LCD_B_OFFSET]
+		LDR R4, =LCD_COLOR_OFF
+		STRH R4, [R0, #LCD_R_OFFSET]
+		STRH R4, [R0, #LCD_G_OFFSET]
+
+		; display on seven segment display
+		LDR R0, =ADDR_7_SEG_BIN_DS3_0
+		STRB R3, [R0]
+
+		; write ascii
+		LDR R0, =ADDR_LCD_ASCII
+		
+test_2_bit
+		CMP R3, #4
+		BGE test_4_bit
+		
+		LDR R4, ="2"
+		STRB R4, [R0, #0]
+		B test_end
+test_4_bit
+		CMP R3, #16
+		BGE over_4_bit
+		
+		LDR R4, ="4"
+		STRB R4, [R0, #0]
+		B test_end
+over_4_bit		
+		LDR R4, ="8"
+		STRB R4, [R0, #0]
+		B test_end
+test_end
+		
+		LDR R4, ="B"
+		STRB R4, [R0, #2]
+
+		LDR R4, ="i"
+		STRB R4, [R0, #3]
+		
+		LDR R4, ="t"
+		STRB R4, [R0, #4]
+
+
+        B          main_loop
+
+
+; ----------------------------------------
+case_red
+		; change display color
+		LDR R0, =ADDR_LCD_COLOUR
+		LDR R4, =LCD_COLOR_ON
+		STRH R4, [R0, #LCD_R_OFFSET]
+		LDR R4, =LCD_COLOR_OFF
+		STRH R4, [R0, #LCD_G_OFFSET]
+		STRH R4, [R0, #LCD_B_OFFSET]
+
+		; display on seven segment display
+		LDR R0, =ADDR_7_SEG_BIN_DS3_0
+		STRB R3, [R0]
+
+; count zeros in diff
+		MVNS R3, R3 ; invert R3 to count zeros
+		MOVS R4, #0 ; value with number of zeros
+		MOVS R5, #0
+count_zeros_loop
+		CMP R3, #0
+		BEQ count_zeros_loop_end
+		
+		LSLS R3, #1
+		ADCS R4, R4, R5
+		
+		B count_zeros_loop
+count_zeros_loop_end
+		LDR R0, =ADDR_LCD_ASCII_2ND_LINE
+		STRB R4, [R0]
+
+
+        B          main_loop
+		
+		
 clear_lcd
         PUSH       {R0, R1, R2}
         LDR        R2, =0x0
---
+++ 8_structured_programming/structogramm.cs
@@ -0,0 +1,38 @@
+// read adc value -> store to register
+// The function call BL adc_get_value will return the ADC-value in the 32-bit register R0. 
+int adcValue = adcGetValue();
+
+if(buttonT0Pressed){
+    setLCDColor(green);
+    displayADCValue(adcValue);
+
+    // display scaled value on led bar
+    int scaledValue = adcValue / 8;
+    int ledMask = 0;
+    for(int i = scaledValue; i > 0; i--){
+        ledMask |= 0x01;
+        ledMask <<= 1;
+    }
+    displayOnLed31to0(ledMask);
+    
+} else {
+    var dipSwitchValueS7toS0 = readDipSwitchValues();
+    var newAdcValue = dipSwitchValueS7toS0 - adcValue;
+
+    if(newAdcValue >= 0){
+        setLCDColor(blue);
+    }else{
+        setLCDColor(red);
+    }
+
+    displayADCValue(newAdcValue);
+}
+
+/*
+constrolling of seven segment displays
+ADDR_SEG7_BIN   EQU      0x60000114
+ 
+                LDR      r0, =ADDR_SEG7_BIN
+                STRB     r1, [r0, #0]                     ; Write byte of binary data to DS1..0.
+                STRH     r1, [r0, #0]                     ; Write half word of binary data to all displays.
+*/
